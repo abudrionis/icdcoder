0b900fb0792209ed4a0723ad3cf72edbc69cc4f7
sonjaremmer <77795246+sonjaremmer@users.noreply.github.com>
2021-09-07 14:53:10 +0200

    Update evaluate.py

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -13,10 +13,14 @@ import numpy as np
 
 def evaluate(pre_trained_model, fine_tuned_model, batch_size, threshold):
 
-
+    # The labels are read from the txt file listing the labels
     data = pd.read_csv('labels.txt', delimiter=',')
     labels = data.columns.to_list()
 
+    # Defining the model
+    bert_model = torch.load(fine_tuned_model)
+
+    # Defining the tokenizer
     bert_tokenizer = AutoTokenizer.from_pretrained(pre_trained_model,
                                                     model_max_length=512,
                                                     max_len=512,
@@ -24,16 +28,19 @@ def evaluate(pre_trained_model, fine_tuned_model, batch_size, threshold):
                                                     padding='Longest'
                                                     )
 
-    bert_model = torch.load(fine_tuned_model)
-
+    # Initializing the trainer
     trainer = Trainer(model=bert_model, tokenizer=bert_tokenizer)
 
+    # Saving the terminal input as input_text
     input_text = input('Enter the discharge summary: ')
 
+    # Predicting the ICD code(s) of the input text. Outputs a list where the first item of the list is a list of the length of number of ICD codes where each element is 0/1 representing if the label is present (1) or not (0)
     predictions = trainer.evaluate(X=[input_text], batch_size=batch_size, thres=threshold, num_labels=len(labels))
 
+    # Creating an empty list to put the ICD code(s) associated with the discharge summary
     prediction_list = []
 
+    # For each prediction and 
     for prediction, label in zip(predictions[0], labels):
         if prediction == 1:
             prediction_list.append(label)
