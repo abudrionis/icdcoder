81a7d8c51ea6c2bbd424ae721803c58f380b43d9
Anastasios Lamproudis <TLampr@gmail.com>
2021-09-24 15:06:46 +0200

    Fix the K-Fold validation set / 2

--- Utilities.py
+++ Utilities.py
@@ -245,7 +245,7 @@ class KFoldCrossVal:
             shuffled_split = ShuffleSplit(n_splits=1, test_size=0.1, random_state=self.random_state)
             for final_train_indices, val_indices in shuffled_split.split(X=x):
                 x, y = [x[i] for i in final_train_indices], [y[i] for i in final_train_indices]
-                x_val, y_val = [x[i] for i in val_indices], [x[i] for i in val_indices]
+                x_val, y_val = [x[i] for i in val_indices], [y[i] for i in val_indices]
             new_trainer.train(X=x, Y=y, X_val=x_val, Y_val=y_val,
                               epochs=epochs,
                               batch_size=batch_size,
